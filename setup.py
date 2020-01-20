015fda76397f828a218f61af92855ea238a72b68
Aaron <aaronelliot27@gmail.com>
2020-01-19 23:46:38 -0500

    * merge requirements.txt into setup.py * modified setup.py to match new directory layout

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 try:
-    from setuptools import setup, Extension
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup, Extension
+    from distutils.core import setup, find_packages
 
-setup(name='slim_python',
-      version='1.0',
+setup(name='slim_python3',
+      version='0.1',
       description='learn optimized scoring systems from data',
       long_description = '''
-    slim-python is a free software package to train SLIM scoring systems
-    using IBM ILOG CPLEX API and the Python programming language. ''',
-      author='Berk Ustun',
-      author_email='ustunb@mit.edu',
-      url='https://www.berkustun.com/',
-      packages=['slim_python'],
+      slim-python is a free software package to train SLIM scoring systems
+                         ''',
+      author='Berk Ustun, Hallee Wong & Aaron Elliot',
+      author_email='aaronelliot27@gmail.com', #'ustunb@mit.edu',
+      url = 'https://github.com/AaEll/slim-python', #url='https://www.berkustun.com/',
+      packages=find_packages('slim_python3'),
+      include_package_data=True,
+      zip_safe=False,
+      python_requires = '>=3.5',
+      install_requires=['ortools',
+                        'numpy',
+                        'scipy',
+                        'pandas',
+                        'ortools',
+                        'PrettyTable'
+                        ''],  
+
+
       )
